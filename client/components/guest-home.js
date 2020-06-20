eba4627a8c829b6385dfe52e9faeb5f411da286f
Madi Knox <madeleinelknox@gmail.com>
2020-06-20 09:32:47 -0400

    chore: added links to navbar and in the process of making style changes

---
+++ client/components/guest-home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GuestHome = () => {
+  return (
+    <div>
+      <h1>NEVER-BORED</h1>
+      <h2>ALWAYS BOARD!</h2>
+      <div>Welcome to the wonderful world of board games!</div>
+    </div>
+  )
+}
+
+export default GuestHome
