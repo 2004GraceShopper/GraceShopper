414625c5f4b74d7c24f467f69db0194c5d2b629b
Madi Knox <madeleinelknox@gmail.com>
2020-06-20 14:47:48 -0400

    chore: first round of style updates for form

--- client/components/GuestCheckoutForm.js
+++ client/components/GuestCheckoutForm.js
@@ -32,48 +32,54 @@ export class GuestCheckoutForm extends React.Component {
     const redirectToConfirmation = this.state.redirectToConfirmation
     if (redirectToConfirmation) return <Redirect to="/" />
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="email">
-            <div>Email Address:</div>
-            <input
-              type="text"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="billingAddress">
-            <div>Billing Address:</div>
-            <input
-              type="text"
-              name="billingAddress"
-              value={this.state.billingAddress}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="shippingAddress">
-            <div>Shipping Address:</div>
-            <input
-              type="text"
-              name="shippingAddress"
-              value={this.state.shippingAddress}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="creditCardNum">
-            <div>Credit Card Number:</div>
-            <input
-              type="text"
-              name="creditCardNum"
-              value={this.state.creditCardNum}
-              onChange={this.handleChange}
-            />
-          </label>
-          <button type="submit" className="submit">
-            Submit
-          </button>
-        </form>
+      <div className="container">
+        <div className="form_sub-container">
+          <h4>Please add your information below:</h4>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="email" className="input-field">
+              <div>Email Address:</div>
+              <input
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label htmlFor="billingAddress" className="input-field">
+              <div>Billing Address:</div>
+              <input
+                type="text"
+                name="billingAddress"
+                value={this.state.billingAddress}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label htmlFor="shippingAddress" className="input-field">
+              <div>Shipping Address:</div>
+              <input
+                type="text"
+                name="shippingAddress"
+                value={this.state.shippingAddress}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label htmlFor="creditCardNum" className="input-field">
+              <div>Credit Card Number:</div>
+              <input
+                type="text"
+                name="creditCardNum"
+                value={this.state.creditCardNum}
+                onChange={this.handleChange}
+              />
+            </label>
+            <div className="submit_button_specs">
+              <button type="submit" className="submit">
+                Submit
+              </button>
+              <div> to review your order</div>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
