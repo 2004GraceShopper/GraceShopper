f2d9558d9d77d89f2521d76086f9981770c523d8
Alison <alison.a.hernandez@gmail.com>
2020-06-22 01:14:47 -0400

    fix & refactor: Changed when carts are saved into state; took some repetitive code and turned it into reusable functions; Carts in state are now eager loaded with product info.

--- server/api/products.js
+++ server/api/products.js
@@ -3,6 +3,7 @@ const {Product, User, Cart} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
+  console.log('**GET api/products**')
   try {
     const products = await Product.findAll()
     res.json(products)
@@ -12,6 +13,7 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:id', async (req, res, next) => {
+  console.log('**GET api/products/:id**')
   try {
     const singleProduct = await Product.findByPk(req.params.id)
     res.json(singleProduct)
