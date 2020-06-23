fd7e0f99e011f05ce9ef41cb4a42d8aaa7d839b8
Alison <alison.a.hernandez@gmail.com>
2020-06-23 16:32:30 -0400

    chore: Added functionality to GuestCheckoutForm submit button; now creates Guest, and redirects if no Sequelize validation errors.

--- server/api/guests.js
+++ server/api/guests.js
@@ -1,24 +1,38 @@
 const router = require('express').Router()
 const {Guest} = require('../db/models')
+const Sequelize = require('sequelize')
 module.exports = router
 
 router.post('/', async (req, res, next) => {
   console.log('**POST api/guests**')
+  console.log('req.login??: ', req.login)
   try {
-    const guest = await Guest.create(req.body)
-    res.status(201).send(guest)
+    Guest.create(req.body)
+      .then(function(guest) {
+        console.log('.then ran!')
+        return res.json(guest)
+      })
+      .catch(Sequelize.ValidationError, function(msg) {
+        console.log("What's the secret message?: ", msg)
+        return res.status(422).send(msg)
+      })
+      .catch(function(err) {
+        next(err)
+      })
+
+    // const helperFunc = (err) => {
+    //   res.json(err)
+    //   next(err)
+    // }
+    // req.login(
+    //   guest,
+    //   err => (err ? res.json(err) : res.json(guest))
+    // )
+    const help = () => {
+      console.log('help!')
+    }
   } catch (error) {
+    res.json(error)
     next(error)
   }
 })
-
-// router.put('/:id/cart', async (req, res, next) => {
-//     try {
-//         const newItem = await Item.create({
-//             quantity: 1,
-//             productId
-//         })
-//     } catch (error) {
-//         next(error)
-//     }
-// })
